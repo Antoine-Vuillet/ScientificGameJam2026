0d32d15b90882e52091ca7a56dce0f2cd3c1424a
Antoine Vuillet <antoine.vuillet@etu.univ-grenoble-alpes.fr>
2026-03-15 13:17:02 +0100

    fix: good features

--- Assets/Script/Scriptable Objects/Feature/FeatureSO.cs
+++ Assets/Script/Scriptable Objects/Feature/FeatureSO.cs
@@ -6,13 +6,12 @@ using NaughtyAttributes;
 [CreateAssetMenu(fileName = "Feature", menuName = "Feature/Feature")]
 public class FeatureSO : ScriptableObject
 {
-    public int price;
     public bool isDark;
     [Header("Objectifs")]
-    [Range(1, 10)]
+    [Range(0, 10)]
     public int autonomy;
-    [Range(1, 10)]
+    [Range(0, 10)]
     public int social;
-    [Range(1, 10)]
+    [Range(0, 10)]
     public int competence;
 }
